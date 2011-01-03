a3e428a1f3d08475c9b632aa0d511f59a2ead6fc
Santosh <santosh.wadghule@gmail.com>
2011-01-04 01:29:05 +0530

    Added year object throughout pages. some changes in UI.

--- mechanicles.rb
+++ mechanicles.rb
@@ -4,15 +4,23 @@ set :app_file, __FILE__
 set :root, File.dirname( __FILE__ )
 set :public, File.dirname( __FILE__ ) + '/public'
 
-get '/' do
-  erb :index
-end
+class Mechanicles
 
-get '/info' do
-  erb :info
-end
+  @@year = Time.now.year
 
-get '/about' do
-  erb :about
-end
+  get '/' do
+    @year = @@year
+    erb :index
+  end
 
+  get '/info' do
+    @year = @@year
+    erb :info
+  end
+
+  get '/about' do
+    @year = @@year
+    erb :about
+  end
+
+end
