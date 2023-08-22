4df454f4616355a955d21c75952d0d81f1b9795d
Santosh Wadghule <santosh.wadghule@gmail.com>
2023-08-22 16:59:32 +0530

    Remove unwanted code

--- mechanicles.rb
+++ mechanicles.rb
@@ -3,11 +3,6 @@
 class Mechanicles < Sinatra::Base
   set :root, File.dirname(__FILE__)
 
-  before do
-    @current_year     = current_year
-    @total_experience = current_year - profession_starting_year
-  end
-
   get '/' do
     erb :home
   end
@@ -18,15 +13,5 @@ class Mechanicles < Sinatra::Base
 
   not_found do
     '404: Not found!'
-  end
-
-  private
-
-  def current_year
-    Time.now.year
-  end
-
-  def profession_starting_year
-    Date.parse('9/8/2010').year
   end
 end
